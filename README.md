eda9f2c0717e617085ccf12b459be1d05d3a6098
Victor <victorchen.3012@gmail.com>
2021-02-23 17:24:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# electron-worker-threads
+This repository is used to reproduce the exceptions raised when worker_thread is used in electron, based on [tutorial](https://www.electronjs.org/docs/tutorial/quick-start#%E5%88%9B%E5%BB%BA%E4%B8%BB%E8%84%9A%E6%9C%AC%E6%96%87%E4%BB%B6).
+
+# Steps to reproduce
+1. `npm install`
+
+2. `npm start`
+
+3. after app is running, press `command + q`
+
+4. View errors in the console
---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const { Worker } = require('worker_threads');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  let script = `console.log('working...');`;
+  new Worker(script, { eval: true });
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
